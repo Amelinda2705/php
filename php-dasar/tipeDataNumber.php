da355e7870258dfa3f9a27cde315ad224d610264
Amelinda <amelindaa2705@gmail.com>
2023-09-24 22:49:09 +0700

    add new file : type data boolean and number

---
+++ php-dasar/tipeDataNumber.php
@@ -0,0 +1,31 @@
+<?php 
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(0123);
+
+echo "Hexadecimal : ";
+var_dump(0x1a);
+
+echo "Binary : ";
+var_dump(0b111111);
+
+echo "Underscore di Number : ";
+var_dump(1_234_5678);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating point dengan E notation Plus : (1.2 x 1.000) : ";
+var_dump(1.2e3);
+
+echo "FLoating point dengan E notation Minus : (1.2 x 0.001) : ";
+var_dump(1.2e-3);
+
+echo "Underscore di floating point : ";
+var_dump(1_234.567);
+
+echo "Overflow integer 64 bit : ";
+var_dump(9223372036854775808); //overflow-nya 9223372036854775807 
