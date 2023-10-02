b50db035e73c2056789fa447fa38bd65d83c93f2
Amelinda <amelindaa2705@gmail.com>
2023-10-02 23:22:13 +0700

    add files : operator penugasan aritmatika dan operator perbandingan

---
+++ php-dasar/operatorPerbandingan.php
@@ -0,0 +1,11 @@
+<?php 
+
+var_dump("10" == 10);
+var_dump("10" === 10);
+var_dump("3" != 10);
+var_dump("10" <> 10);
+var_dump(10 !== 3);
+var_dump(10 > 8);
+var_dump(10 >= 8);
+var_dump(10 < 10);
+var_dump(10 <= 10);
