a70c4a50ae589b6a0e4a8f7d23c9e3ad7eaa6b44
Amelinda <amelindaa2705@gmail.com>
2023-10-02 22:41:38 +0700

    add new file : aritmaika.php

---
+++ php-dasar/aritmatika.php
@@ -0,0 +1,17 @@
+<?php 
+
+$a = 10;
+$b = 20;
+
+$hasilpertambahan = $a + $b;
+echo $hasilpertambahan . "\n";
+
+$hasilperkurangan = $a - $b;
+echo $hasilperkurangan . "\n";
+echo -$hasilperkurangan . "\n";
+
+$hasilmodulus = $a % $b;
+echo $hasilmodulus . "\n";
+
+$hasilpangkat = $a ** 2;
+echo $hasilpangkat;
