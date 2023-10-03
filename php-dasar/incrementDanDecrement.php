fa2bc4185358a595d4670e3ff56ff6b74087969c
Amelinda <amelindaa2705@gmail.com>
2023-10-04 04:42:41 +0700

    add 2 new files about operator logika, increment, and decrement

---
+++ php-dasar/incrementDanDecrement.php
@@ -0,0 +1,44 @@
+<?php 
+
+// increment
+
+$a = 10;
+
+// post increment
+$b = $a++;
+// $b = $a;
+// $a = $a + 1;
+
+var_dump($a);
+var_dump($b);
+
+$a = 10;
+
+// pre increment 
+$b = ++$a;
+// $a = $a + 1;
+// $b = $a;
+
+var_dump($a);
+var_dump($b);
+
+// decrement 
+
+$a = 10;
+// post decrement
+$b = $a--;
+// $b = $a;
+// $a = $a + 1;
+
+var_dump($a);
+var_dump($b);
+
+$a = 10;
+
+// pre decrement 
+$b = --$a;
+// $a = $a - 1;
+// $b = $a;
+
+var_dump($a);
+var_dump($b);
