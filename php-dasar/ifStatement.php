52a2f1d4182de29c8291f16480e2016f2b09743b
Amelinda <amelindaa2705@gmail.com>
2023-10-12 23:24:57 +0700

    edit ifStatement.php file with another method of if and edit switch.php with another method of switch

--- php-dasar/ifStatement.php
+++ php-dasar/ifStatement.php
@@ -14,3 +14,17 @@ if ($nilai >= 80 && $absen >= 80) {
 } else {
     echo "Nilai anda E" . PHP_EOL;
 }
+
+$nilai = "70";
+
+if ($nilai >= 80 && $absen >= 80) :
+    echo "Nilai anda A" . PHP_EOL;
+    elseif ($nilai >= 70 && $absen >= 70) :
+    echo "Nilai anda B" . PHP_EOL;
+    elseif ($nilai >= 60 && $absen >= 60) :
+    echo "Nilai anda C" . PHP_EOL;
+    elseif ($nilai >= 50 && $absen >= 50) :
+    echo "Nilai anda D" . PHP_EOL;
+    else :
+    echo "Nilai anda E" . PHP_EOL;
+endif;
--- php-dasar/switch.php
+++ php-dasar/switch.php
@@ -6,13 +6,30 @@ switch ($nilai) {
     case "A":
         echo "Anda lulus dengan sangat baik" . PHP_EOL;
         break;
-    case "C":
     case "B":
+    case "C":
         echo "Anda Lulus" . PHP_EOL;
         break;
     case "D":
         echo "Anda tidak Lulus" . PHP_EOL;
         break;
     default: 
-    echo "Manusia salah jurusan" . PHP_EOL; 
+    echo "Mungkin anda salah jurusan" . PHP_EOL; 
 }
+
+$nilai = "a";
+
+switch ($nilai) :
+    case "A":
+        echo "Anda lulus dengan sangat baik" . PHP_EOL;
+        break;
+    case "B":
+    case "C":
+        echo "Anda Lulus" . PHP_EOL;
+        break;
+    case "D":
+        echo "Anda tidak Lulus" . PHP_EOL;
+        break;
+    default: 
+    echo "Mungkin anda salah jurusan" . PHP_EOL; 
+    endswitch;
