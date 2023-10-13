8e8fb5b81404dc7e20a75a6c1ef22fb9e7b0f9f5
Amelinda <amelindaa2705@gmail.com>
2023-10-13 20:08:07 +0700

    add new file about ternary operator

---
+++ php-dasar/ternaryOperator.php
@@ -0,0 +1,12 @@
+<?php 
+
+$gender = "PRIA";
+$hi = $gender == "PRIA" ? "Hi, Bro!" : "Hi, Nona!";
+
+// if ($gender == "PRIA") {
+//     $hi = "Hi Bro!";
+// } else {
+//     $hi = "Hi Nona!";
+// }
+
+echo $hi . PHP_EOL;
