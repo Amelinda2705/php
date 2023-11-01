edeca314a046e8b192294c5d83d2ff70d5353efb
Amelinda <amelindaa2705@gmail.com>
2023-11-01 21:23:38 +0700

    add new file about string function

---
+++ php-dasar/stringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(", ", [10, 11, 12, 13, 14, 15]));
+var_dump(explode(" ", "Eko Kurniawan Khannedy"));
+var_dump(strtolower("AMELINDA"));
+var_dump(strtoupper("amelinda"));
+var_dump(trim("         amel   linda         "));
+var_dump(substr("Amelinda", 0, 1));
